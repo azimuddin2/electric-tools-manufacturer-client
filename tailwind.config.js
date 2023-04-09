12933d004b3d57c0ed1ccb70d3b75a5ba9ee2788
azimuddin2 <mohammadazimuddin274@gmail.com>
2023-04-09 11:54:27 +0600

    project structure

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: [
+      {
+        electrictoolstheme: {
+          primary: "black",
+          secondary: "#1e2b47",
+          accent: "#19223c",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+      "dark",
+      "cupcake",
+    ],
+  },
+  plugins: [require("daisyui")],
+}
+
