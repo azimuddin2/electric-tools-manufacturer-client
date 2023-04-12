80e93fc2f98b5aa341ab0b0595eca824a195c70b
azimuddin2 <mohammadazimuddin274@gmail.com>
2023-04-12 17:25:16 +0600

    business overview number counter

--- src/pages/Home/BusinessOverview/BusinessOverview.js
+++ src/pages/Home/BusinessOverview/BusinessOverview.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import backgroundImg from '../../../assets/images/background.jpg';
 import { FaTools, FaUsers } from 'react-icons/fa';
 import { AiTwotoneLike, AiFillDollarCircle } from 'react-icons/ai';
+import CountUp from 'react-countup';
+import ScrollTrigger from 'react-scroll-trigger';
 
 const BusinessOverview = () => {
+    const [counter, setCounter] = useState(false);
+
     return (
         <section style={{
             background: `url(${backgroundImg})`,
@@ -22,7 +26,12 @@ const BusinessOverview = () => {
                 <div className="stats shadow">
                     <div className="stat text-center">
                         <FaTools className='text-primary text-6xl mb-2 mx-auto'></FaTools>
-                        <h2 className="stat-value">200k</h2>
+                        <ScrollTrigger onEnter={() => setCounter(true)} onExit={() => setCounter(false)}>
+                            <h2 className="stat-value">
+                                {counter && <CountUp start={1} end={100} duration={2} delay={0}></CountUp>}
+                                k
+                            </h2>
+                        </ScrollTrigger>
                         <h3 className="text-lg text-primary font-medium">Tools</h3>
                     </div>
                 </div>
@@ -30,7 +39,12 @@ const BusinessOverview = () => {
                 <div className="stats shadow">
                     <div className="stat text-center">
                         <AiFillDollarCircle className='text-primary text-6xl mb-2 mx-auto'></AiFillDollarCircle>
-                        <h2 className="stat-value">200M</h2>
+                        <ScrollTrigger onEnter={() => setCounter(true)} onExit={() => setCounter(false)}>
+                            <h2 className="stat-value">
+                                {counter && <CountUp start={1} end={200} duration={2} delay={0}></CountUp>}
+                                M
+                            </h2>
+                        </ScrollTrigger>
                         <h3 className="text-lg text-primary font-medium">Annual Revenue</h3>
                     </div>
                 </div>
@@ -38,7 +52,12 @@ const BusinessOverview = () => {
                 <div className="stats shadow">
                     <div className="stat text-center">
                         <FaUsers className='text-primary text-6xl mb-2 mx-auto'></FaUsers>
-                        <h2 className="stat-value">120k</h2>
+                        <ScrollTrigger onEnter={() => setCounter(true)} onExit={() => setCounter(false)}>
+                            <h2 className="stat-value">
+                                {counter && <CountUp start={1} end={120} duration={2} delay={0}></CountUp>}
+                                k
+                            </h2>
+                        </ScrollTrigger>
                         <h3 className="text-lg text-primary font-medium"> Customers</h3>
                     </div>
                 </div>
@@ -46,7 +65,12 @@ const BusinessOverview = () => {
                 <div className="stats shadow">
                     <div className="stat text-center">
                         <AiTwotoneLike className='text-primary text-6xl mb-2 mx-auto'></AiTwotoneLike>
-                        <h2 className="stat-value">44k</h2>
+                        <ScrollTrigger onEnter={() => setCounter(true)} onExit={() => setCounter(false)}>
+                            <h2 className="stat-value">
+                                {counter && <CountUp start={1} end={44} duration={2} delay={0}></CountUp>}
+                                k
+                            </h2>
+                        </ScrollTrigger>
                         <h3 className="text-lg text-primary font-medium">Feedbacks</h3>
                     </div>
                 </div>
