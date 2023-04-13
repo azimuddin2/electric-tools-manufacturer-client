1aa26bc9c74deef5604c0f410c3732f36354b684
azimuddin2 <mohammadazimuddin274@gmail.com>
2023-04-14 00:01:28 +0600

    updated home

--- src/pages/Home/Home/Home.js
+++ src/pages/Home/Home/Home.js
@@ -3,6 +3,8 @@ import Banner from '../Banner/Banner';
 import CompanyLogo from '../CompanyLogo/CompanyLogo';
 import Tools from '../Tools/Tools';
 import BusinessOverview from '../BusinessOverview/BusinessOverview';
+import Contact from '../Contact/Contact';
+import Testimonials from '../Testimonial/Testimonials';
 
 const Home = () => {
     return (
@@ -11,6 +13,8 @@ const Home = () => {
             <CompanyLogo></CompanyLogo>
             <Tools></Tools>
             <BusinessOverview></BusinessOverview>
+            <Contact></Contact>
+            <Testimonials></Testimonials>
         </div>
     );
 };
