65a05925a5c77e5aa938c677d68eb22ac206438a
azimuddin2 <mohammadazimuddin274@gmail.com>
2023-05-01 23:49:29 +0600

    added a order page

---
+++ src/pages/Dashboard/MyOrders/MyOrders.js
@@ -0,0 +1,77 @@
+import { signOut } from 'firebase/auth';
+import React, { useEffect, useState } from 'react';
+import { useContext } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { AuthContext } from '../../../contexts/AuthProvider/AuthProvider';
+import useTitle from '../../../hooks/useTitle';
+import { useQuery } from '@tanstack/react-query';
+import Loading from '../../Shared/Loading/Loading';
+
+const MyOrders = () => {
+    useTitle('My Orders');
+    const { user } = useContext(AuthContext);
+    // const [payment, setPayment] = useState(null);
+
+    const url = `http://localhost:5000/order?customerEmail=${user.email}`;
+
+    const { data: orders, isLoading, refetch } = useQuery({
+        queryKey: ['bookings', user?.email],
+        queryFn: async () => {
+            try {
+                const res = await fetch(url, {
+                    headers: {
+                        authorization: `bearer ${localStorage.getItem('accessToken')}`
+                    }
+                })
+                const data = await res.json();
+                return data;
+            }
+            catch (error) {
+
+            }
+        }
+    })
+
+    if (isLoading) {
+        return <Loading></Loading>
+    }
+
+    return (
+        <div>
+            <div className="overflow-x-auto">
+                <table className="table w-full">
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Name</th>
+                            <th>Quantity</th>
+                            <th>Price</th>
+                            <th>Payment</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            orders.map((order, index) => <tr key={order._id}>
+                                <th>{index + 1}</th>
+                                <td>{order.toolName}</td>
+                                <td>{order.orderQuantity}</td>
+                                <td>${order.toolPrice}</td>
+                                <td>
+                                    {(order.toolPrice && !order.paid) && <Link to={`/dashboard/payment/${order._id}`}><button className='btn btn-xs btn-success'>Pay</button></Link>}
+                                    {(order.toolPrice && order.paid) && <div>
+                                        <p><span className='text-success'>Paid</span> </p>
+                                        <p>Transaction id: <span className='text-success'>{order.transactionId}</span> </p>
+                                    </div>}
+
+                                </td>
+
+                            </tr>)
+                        }
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    );
+};
+
+export default MyOrders;
