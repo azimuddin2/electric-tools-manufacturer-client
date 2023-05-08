70e9ff5be684b28e11061e97ca5caef7ad85de1e
azimuddin2 <mohammadazimuddin274@gmail.com>
2023-05-08 23:55:40 +0600

    added a manage product

---
+++ src/pages/Dashboard/ManageProducts/ManageProducts.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import Loading from '../../Shared/Loading/Loading';
+import { useQuery } from '@tanstack/react-query';
+import { useState } from 'react';
+
+const ManageProducts = () => {
+    const [deletingProduct, setDeletingProduct] = useState(null);
+    const { data: tools, isLoading, refetch } = useQuery('tools', () => fetch('http://localhost:5000/tool', {
+        headers: {
+            authorization: `Bearer ${localStorage.getItem('accessToken')}`
+        }
+    }).then(res => res.json()));
+
+    if (isLoading) {
+        return <Loading></Loading>
+    }
+    return (
+        <div>
+            <h1>Manage Products: {tools.length}</h1>
+            <div className="overflow-x-auto">
+                <table className="table w-full">
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Avatar</th>
+                            <th>Name</th>
+                            <th>Price</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            tools.map((tool, index) => <ProductRow
+                                key={tool._id}
+                                tool={tool}
+                                index={index}
+                                refetch={refetch}
+                                setDeletingProduct={setDeletingProduct}
+                            ></ProductRow>)
+                        }
+                    </tbody>
+                </table>
+            </div>
+            {
+                deletingProduct && <DeleteConfirmModal
+                    deletingProduct={deletingProduct}
+                    refetch={refetch}
+                    setDeletingProduct={setDeletingProduct}
+                ></DeleteConfirmModal>
+            }
+        </div>
+    );
+};
+
+export default ManageProducts;
