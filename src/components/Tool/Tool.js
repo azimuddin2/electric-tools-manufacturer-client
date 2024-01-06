f2b0ef74c0584a5f5e9407e9ba7fffde6cf48c25
azimuddin2 <mohammadazimuddin274@gmail.com>
2024-01-06 04:27:26 +0000

    updated tool and review file

--- src/components/Tool/Tool.js
+++ src/components/Tool/Tool.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { MdOutlineShoppingCart } from "react-icons/md";
 import { useNavigate } from 'react-router-dom';
-import Fade from 'react-reveal/Fade';
 import StarRatings from 'react-star-ratings';
 
 const Tool = ({ tool }) => {
@@ -9,34 +8,32 @@ const Tool = ({ tool }) => {
     const navigate = useNavigate();
 
     return (
-        <Fade bottom>
-            <div className="card lg:shadow-none shadow hover:shadow">
-                <figure>
-                    <img className='w-56 mt-1 mx-auto' src={image} alt={name} />
-                </figure>
-                <div className="card-body">
-                    <h2 className="card-title text-secondary">{name}</h2>
-                    <p className='font-medium text-secondary'>Price: <span className=' text-primary'>${price}</span></p>
-                    <div className="mb-1">
-                        <StarRatings
-                            rating={rating}
-                            starRatedColor="#ff9800"
-                            name="rating"
-                            starSpacing="1px"
-                            starDimension="19px"
-                        />
-                    </div>
-                    <div className="card-actions justify-start">
-                        <button
-                            onClick={() => navigate(`/tool/${_id}`)}
-                            className='btn btn-sm btn-primary text-white capitalize rounded-md'
-                        >
-                            Buy Now <MdOutlineShoppingCart className='text-xl ml-1' />
-                        </button>
-                    </div>
+        <div className="card lg:shadow-none shadow hover:shadow">
+            <figure>
+                <img className='w-56 mt-1 mx-auto' src={image} alt={name} />
+            </figure>
+            <div className="card-body">
+                <h2 className="card-title text-secondary">{name}</h2>
+                <p className='font-medium text-secondary'>Price: <span className=' text-primary'>${price}</span></p>
+                <div className="mb-1">
+                    <StarRatings
+                        rating={rating}
+                        starRatedColor="#ff9800"
+                        name="rating"
+                        starSpacing="1px"
+                        starDimension="19px"
+                    />
+                </div>
+                <div className="card-actions justify-start">
+                    <button
+                        onClick={() => navigate(`/tool/${_id}`)}
+                        className='btn btn-sm btn-primary text-white capitalize rounded-md'
+                    >
+                        Buy Now <MdOutlineShoppingCart className='text-xl ml-1' />
+                    </button>
                 </div>
             </div>
-        </Fade>
+        </div>
     );
 };
 
--- src/pages/Reviews/Review.js
+++ src/pages/Reviews/Review.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import quote from '../../assets/icons/quote.svg';
 import ReactStars from "react-rating-stars-component";
+import Fade from 'react-reveal/Fade';
 
 const Review = ({ review }) => {
     const { name, image, country, description, rating } = review;
@@ -14,37 +15,39 @@ const Review = ({ review }) => {
     };
 
     return (
-        <div className="card p-7 shadow">
-            <div className='flex items-center justify-between'>
-                <div className='md:flex items-center'>
-                    {
-                        image === null ?
-                            <div className="avatar placeholder">
-                                <div className="bg-secondary text-white rounded-full w-12 ring ring-primary ring-offset-base-100 ring-offset-2">
-                                    <span className="text-3xl">{name.slice(0, 1)}</span>
+        <Fade bottom>
+            <div className="card p-7 shadow">
+                <div className='flex items-center justify-between'>
+                    <div className='md:flex items-center'>
+                        {
+                            image === null ?
+                                <div className="avatar placeholder">
+                                    <div className="bg-secondary text-white rounded-full w-12 ring ring-primary ring-offset-base-100 ring-offset-2">
+                                        <span className="text-3xl">{name.slice(0, 1)}</span>
+                                    </div>
                                 </div>
-                            </div>
-                            :
-                            <div className="avatar">
-                                <div className="w-12 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
-                                    <img src={image} alt={name} />
+                                :
+                                <div className="avatar">
+                                    <div className="w-12 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+                                        <img src={image} alt={name} />
+                                    </div>
                                 </div>
-                            </div>
-                    }
-                    <div className='ml-5 mt-2 lg:mt-0'>
-                        <h2 className='text-base lg:text-xl font-bold text-neutral'>{name}</h2>
-                        <p className='font-semibold text-accent'>{country}</p>
+                        }
+                        <div className='ml-5 mt-2 lg:mt-0'>
+                            <h2 className='text-base lg:text-xl font-bold text-neutral'>{name}</h2>
+                            <p className='font-semibold text-accent'>{country}</p>
+                        </div>
                     </div>
+                    <figure>
+                        <img src={quote} alt="Shoes" className='w-12' />
+                    </figure>
                 </div>
-                <figure>
-                    <img src={quote} alt="Shoes" className='w-12' />
-                </figure>
+                <div className="mt-4">
+                    <p className='text-sm text-accent capitalize leading-6'>{description}</p>
+                    <p><ReactStars value={rating} {...thirdExample} /></p>
+                </div>
             </div>
-            <div className="mt-4">
-                <p className='text-sm text-accent capitalize leading-6'>{description}</p>
-                <p><ReactStars value={rating} {...thirdExample} /></p>
-            </div>
-        </div>
+        </Fade>
     );
 };
 
