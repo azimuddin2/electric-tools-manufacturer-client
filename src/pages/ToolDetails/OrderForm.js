875a43d5f59531eb39e6ed90bdace880481cce6c
azimuddin2 <mohammadazimuddin274@gmail.com>
2024-01-07 07:09:46 +0000

    complete toolDetails and orderForm task

--- src/pages/ToolDetails/OrderForm.js
+++ src/pages/ToolDetails/OrderForm.js
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import { AuthContext } from '../../contexts/AuthProvider/AuthProvider';
 import { toast } from 'react-toastify';
 import { useNavigate } from 'react-router-dom';
+import { MdErrorOutline } from 'react-icons/md';
 
 const OrderForm = ({ tool }) => {
     const { user } = useContext(AuthContext);
@@ -10,9 +11,9 @@ const OrderForm = ({ tool }) => {
     const [check, setCheck] = useState(0);
     const navigate = useNavigate();
 
-    const handleOrder = event => {
+    const handleSubmit = (event) => {
         event.preventDefault();
-        const order = {
+        const orderInfo = {
             toolId: _id,
             toolName: name,
             toolPrice: price,
@@ -28,7 +29,7 @@ const OrderForm = ({ tool }) => {
             headers: {
                 'content-type': 'application/json'
             },
-            body: JSON.stringify(order)
+            body: JSON.stringify(orderInfo)
         })
             .then(res => res.json())
             .then(data => {
@@ -41,93 +42,85 @@ const OrderForm = ({ tool }) => {
 
     return (
         <div className='flex justify-center'>
-            <div className=" w-full">
+            <div className="w-full">
                 <div className="card-body p-5 lg:p-8">
-                    <h2 className="text-center text-primary font-medium text-3xl mb-2">Please Order</h2>
+                    <h2 className="text-center text-secondary font-medium text-3xl mb-2">Please Order</h2>
                     <form
-                        onSubmit={handleOrder}
+                        onSubmit={handleSubmit}
                         className='grid grid-cols-1 gap-4 justify-items-center my-6'
                     >
-                        <div className="form-control w-full max-w-md">
+                        <div className="form-control w-full">
                             <input
                                 type="text"
                                 name='name'
                                 disabled
                                 defaultValue={user?.displayName || ''}
-                                className="input input-bordered w-full max-w-md"
+                                className="input input-bordered w-full"
                             />
                         </div>
-
-                        <div className="form-control w-full max-w-md">
+                        <div className="form-control w-full">
                             <input
                                 type="email"
                                 name='email'
                                 disabled
                                 defaultValue={user?.email || ''}
-                                className="input input-bordered w-full max-w-md"
+                                className="input input-bordered w-full"
                             />
                         </div>
-
-                        <div className="form-control w-full max-w-md">
+                        <div className="form-control w-full">
                             <input
                                 type="text"
                                 name='address'
                                 placeholder="Address"
                                 required
-                                className="input input-bordered w-full max-w-md focus:outline-none focus:border-primary focus:ring-1 focus:ring-primary"
+                                className="input input-bordered w-full focus:outline-none focus:border-primary focus:ring-1 focus:ring-primary"
                             />
                         </div>
-
-                        <div className="form-control w-full max-w-md">
+                        <div className="form-control w-full">
                             <input
                                 type="text"
                                 name='phone'
                                 placeholder="Phone Number"
                                 required
-                                className="input input-bordered w-full max-w-md focus:outline-none focus:border-primary focus:ring-1 focus:ring-primary"
+                                className="input input-bordered w-full focus:outline-none focus:border-primary focus:ring-1 focus:ring-primary"
                             />
                         </div>
-
-                        <div className="form-control w-full max-w-md">
+                        <div className="form-control w-full">
                             <input
                                 type="text"
                                 name='tool'
                                 value={name}
                                 disabled
-                                className="input input-bordered w-full max-w-md"
+                                className="input input-bordered w-full"
                             />
                         </div>
-
-                        <div className="form-control w-full max-w-md">
+                        <div className="form-control w-full">
                             <input
                                 type='text'
                                 name='price'
                                 value={`Price: $${price}`}
                                 disabled
-                                className="input input-bordered w-full max-w-md"
+                                className="input input-bordered w-full"
                             />
                         </div>
-
-                        <div className="form-control w-full max-w-md">
+                        <div className="form-control w-full">
                             <input
                                 onChange={(event) => setCheck(event.target.value)}
                                 type="number"
                                 name='quantity'
                                 placeholder='Order Quantity'
-                                className="input input-bordered w-full max-w-md focus:outline-none focus:border-primary focus:ring-1 focus:ring-primary"
+                                className="input input-bordered w-full focus:outline-none focus:border-primary focus:ring-1 focus:ring-primary"
                             />
                             {
-                                check < minimumQuantity && <span className="label-text-alt text-red-500 text-sm text-center"> Minimum Order Quantity/ 0{minimumQuantity}</span>
+                                check < minimumQuantity && <span className="label-text-alt text-red-500 font-medium text-sm flex items-center mt-2 ml-2"><MdErrorOutline className='text-xl' /> Minimum Order Quantity/ 0{minimumQuantity}</span>
                             }
                             {
-                                check > availableQuantity && <span className="label-text-alt text-red-600 text-sm text-center">Available Order Quantity/ {availableQuantity}</span>
-
+                                check > availableQuantity && <span className="label-text-alt text-red-500 font-medium text-sm flex items-center mt-2 ml-2"><MdErrorOutline className='text-xl' /> Available Order Quantity/ {availableQuantity}</span>
                             }
                         </div>
-
                         <input
                             disabled={check < minimumQuantity || check > availableQuantity}
-                            className='btn btn-primary text-white w-full max-w-md mt-4'
+                            className='btn btn-primary text-white w-full mt-3'
                             type="submit"
                             value='Order'
                         />
--- src/pages/ToolDetails/ToolDetails.js
+++ src/pages/ToolDetails/ToolDetails.js
@@ -5,6 +5,7 @@ import useTitle from '../../hooks/useTitle';
 import { useQuery } from '@tanstack/react-query';
 import Loading from '../Shared/Loading/Loading';
 import ErrorMessage from '../Shared/ErrorMessage/ErrorMessage';
+import StarRatings from 'react-star-ratings';
 
 const ToolDetails = () => {
     useTitle('Tool Details');
@@ -30,25 +31,32 @@ const ToolDetails = () => {
     }
 
     return (
-        <section className='max-w-screen-lg lg:mx-auto py-20 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-8 px-4 lg:px-12'>
-            <div className="rounded-xl shadow-lg">
-                <figure>
-                    <img src={image} alt={name} className='mx-auto' />
-                </figure>
-                <div className="card-body">
-                    <h2 className="card-title text-xl">{name}</h2>
-                    <p className=' font-medium'>Price: <span className=' text-primary'>${price}</span></p>
-                    <p className='py-2'>{description}</p>
-                    <p>Minimum Order Quantity: {minimumQuantity}</p>
-                    <p>Available Order Quantity: {availableQuantity}</p>
-                    <div className="rating rating-xs flex items-center">
-                        <input type="radio" name="rating-6" className="pl-6 mask mask-star-2 bg-primary" />
-                        {rating}
+        <section className='max-w-screen-lg lg:mx-auto mx-5 my-12 lg:my-20'>
+            <div className='grid grid-cols-1 lg:grid-cols-2 gap-6'>
+                <div className="rounded-xl shadow pt-10">
+                    <figure>
+                        <img src={image} alt={name} className='mx-auto' />
+                    </figure>
+                    <div className="card-body">
+                        <h2 className="card-title text-2xl text-secondary">{name}</h2>
+                        <p className='font-medium text-secondary text-lg'>Price: <span className='text-primary'>${price}</span></p>
+                        <p className='py-2 text-accent'>{description}</p>
+                        <p>Minimum Order Quantity: 0{minimumQuantity}</p>
+                        <p>Available Order Quantity: {availableQuantity}</p>
+                        <div className="mb-1">
+                            <StarRatings
+                                rating={rating}
+                                starRatedColor="#ff9800"
+                                name="rating"
+                                starSpacing="1px"
+                                starDimension="19px"
+                            />
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div className='card shadow-lg'>
-                <OrderForm tool={tool}></OrderForm>
+                <div className='card shadow'>
+                    <OrderForm tool={tool}></OrderForm>
+                </div>
             </div>
         </section>
     );
